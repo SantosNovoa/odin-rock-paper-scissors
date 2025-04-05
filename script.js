cb18f8a4859e7fbe280270ac677adf94ba581fe9
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-04 20:50:19 -0400

    Fixed a bug for functions getComputerChoice and getHumanChoice
    Both were functions were incorrectly returning undefined instead of their desired string values. I fixed this by instead of returning the console.log, I would return the value itself, giving me the desired string

--- script.js
+++ script.js
@@ -9,17 +9,16 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Enter rock, paper, or scissors!")
-    return choice;
+    return capitalizeFirstLetter(choice);
 }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+}
   
 
 function playRound(humanSelection, computerSelection) {
 
-    capitalizeFirstLetter(humanSelection);
     
     if (humanSelection == "Rock" && computerSelection == "Paper") {
         computerScore++;
@@ -29,47 +28,42 @@ function playRound(humanSelection, computerSelection) {
         humanScore++;
         console.log("You won that round.");
     }
-    else if ((humanSelection == "rock" || "Rock") && computerSelection == "Rock") {
+    else if (humanSelection == "Rock" && computerSelection == "Rock") {
         console.log("It's a draw.");
     }
-    else if ((humanSelection == "paper" || "Paper") && computerSelection == "Scissors") {
+    else if (humanSelection == "Paper" && computerSelection == "Scissors") {
         computerScore++;
         console.log("You lost that round!");
     }
-    else if ((humanSelection == "paper" || "Paper") && computerSelection == "Rock") {
+    else if (humanSelection == "Paper" && computerSelection == "Rock") {
         humanScore++;
         console.log("You won that round.");
     }
-    else if ((humanSelection == "paper" || "Paper") && computerSelection == "Paper") {
+    else if (humanSelection == "Paper" && computerSelection == "Paper") {
         console.log("It's a draw.");
     }
-    else if ((humanSelection == "scissors" || "Scissors") && computerSelection == "Rock") {
+    else if (humanSelection == "Scissors" && computerSelection == "Rock") {
         computerScore++;
         console.log("You lost that round!");
     }
-    else if ((humanSelection == "scissors" || "Scissors") && computerSelection == "Paper") {
+    else if (humanSelection == "Scissors" && computerSelection == "Paper") {
         humanScore++;
         console.log("You won that round.");
     }
-    else if ((humanSelection == "scissors" || "Scissors") && computerSelection == "Scissors") {
+    else if (humanSelection == "Scissors" && computerSelection == "Scissors") {
         console.log("It's a draw.");
     }
     
 }
 
-const humanSelection = getHumanChoice();
+let humanSelection = getHumanChoice();
 
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
 playRound(humanSelection, computerSelection);
 playRound(humanSelection, computerSelection);
 
-console.log("scissors" === "rock" || "Rock");
-
-
-console.log(getHumanChoice());
-console.log(getComputerChoice());
 
 console.log(computerScore);
 console.log(humanScore);
