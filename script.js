71f716f5d682a945647a9ef78aee43f12732d951
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-06 21:00:58 -0400

    Changed all the console.logs into DOM methods

--- script.js
+++ script.js
@@ -23,19 +23,35 @@ let humanSelection = "";
 
 const buttons = document.querySelectorAll("button");
 
+const terminalScreen = document.querySelector(".fakeScreen");
+console.log(terminalScreen);
+
+const winner = document.createElement("div");
+winner.classList.add("winner");
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-
     humanSelection = `${button.className}`;
     let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection)
+    playRound(humanSelection, computerSelection);
 
     if (computerScore == 5) {
-      console.log("\nComputer won.");
+      
+      winner.textContent = "\nComputer won.";
+      terminalScreen.appendChild(winner);
+
+      buttons.forEach(function (button) {
+        button.disabled = true;
+      });
     } else if (humanScore == 5) {
-      console.log("\nYou won!");
+      
+      winner.textContent = "\nYou won!";
+      terminalScreen.appendChild(winner);
+
+      buttons.forEach(function (button) {
+        button.disabled = true;
+      });
     }
-    
   });
 });
 
@@ -63,55 +79,40 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanSelection, computerSelection) {
-  console.log(
-    `\nYou chose ${humanSelection} computer chose ${computerSelection}`
-  );
+
+  const content = document.createElement("div");
+  content.classList.add("content");
+  terminalScreen.appendChild(content);
+
+  const choiceHistory = document.createElement("div");
+  choiceHistory.classList.add("choiceHistory");
+  choiceHistory.textContent = `>\nYou chose ${humanSelection} computer chose ${computerSelection}`;
+  terminalScreen.appendChild(choiceHistory)
 
   if (humanSelection == "rock" && computerSelection == "paper") {
-    computerScore++;
-    console.log(
-      `You lost that round.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      computerScore++;
+      content.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanSelection == "rock" && computerSelection == "scissors") {
-    humanScore++;
-    console.log(
-      `You won that round!\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      humanScore++;
+      content.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanSelection == "rock" && computerSelection == "rock") {
-    console.log(
-      `It's a draw.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      content.textContent = `It's a draw. Your score: ${humanScore}, Computer score: ${computerScore}`;
   } else if (humanSelection == "paper" && computerSelection == "scissors") {
-    computerScore++;
-    console.log(
-      `You lost that round.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      computerScore++;
+      content.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
   } else if (humanSelection == "paper" && computerSelection == "rock") {
-    humanScore++;
-    console.log(
-      `You won that round!\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      humanScore++;
+      content.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
   } else if (humanSelection == "paper" && computerSelection == "paper") {
-    console.log(
-      `It's a draw.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      content.textContent =  `It's a draw. Your score: ${humanScore} \nComputer score: ${computerScore}`
   } else if (humanSelection == "scissors" && computerSelection == "rock") {
-    computerScore++;
-    console.log(
-      `You lost that round.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      computerScore++;
+      content.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
   } else if (humanSelection == "scissors" && computerSelection == "paper") {
-    humanScore++;
-    console.log(
-      `You won that round!\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
-  } else if (
-    humanSelection == "scissors" &&
-    computerSelection == "scissors"
-  ) {
-    console.log(
-      `It's a draw.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-    );
+      humanScore++;
+      content.textContent = `Your score: ${humanScore}, Computer score: ${computerScore}`
+  } else if (humanSelection == "scissors" && computerSelection == "scissors") {
+      content.textContent = `It's a draw. Your score: ${humanScore}, Computer score: ${computerScore}`
   }
 }
 
