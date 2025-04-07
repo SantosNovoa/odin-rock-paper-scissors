98be546142218c6017e26233cef357c95fd7a282
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-06 21:47:09 -0400

    Added a function that auto-scrolls the terminal when new score data is appended

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-// const rock = document.querySelector(".rock");
-// const paper = document.querySelector(".paper");
-// const scissors = document.querySelector(".scissors");
 
-// function alertFunctionRock() {
-//   alert("You chose rock.");
-// }
-
-// function alertFunctionPaper() {
-//   alert("You chose paper.");
-// }
-
-// function alertFunctionScissors() {
-//   alert("You chose scissors.");
-// }
-
-// rock.addEventListener("click", alertFunctionRock);
-// paper.addEventListener("click", alertFunctionPaper);
-// scissors.addEventListener("click", alertFunctionScissors);
-
-// let myString = "";
 let humanSelection = "";
 
 const buttons = document.querySelectorAll("button");
 
-const terminalScreen = document.querySelector(".fakeScreen");
-console.log(terminalScreen);
 
-const winner = document.createElement("div");
-winner.classList.add("winner");
+const terminalScreen = document.querySelector(".fakeScreen");
+
+terminalScreen.scrollTop = terminalScreen.scrollHeight;
+
+// function to autoscroll when new score data is outputted to the terminal
+window.setInterval(function() {
+  terminalScreen.scrollTop = terminalScreen.scrollHeight;
+}, 100);
+
+const winnerComputer = document.createElement("div");
+winnerComputer.classList.add("winnerComputer");
+
+const winnerHuman = document.createElement("div");
+winnerHuman.classList.add("winnerHuman");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -37,16 +27,16 @@ buttons.forEach((button) => {
 
     if (computerScore == 5) {
       
-      winner.textContent = "\nComputer won.";
-      terminalScreen.appendChild(winner);
+      winnerComputer.textContent = "\nComputer won.";
+      terminalScreen.appendChild(winnerComputer);
 
       buttons.forEach(function (button) {
         button.disabled = true;
       });
     } else if (humanScore == 5) {
       
-      winner.textContent = "\nYou won!";
-      terminalScreen.appendChild(winner);
+      winnerHuman.textContent = "\nYou won!";
+      terminalScreen.appendChild(winnerHuman);
 
       buttons.forEach(function (button) {
         button.disabled = true;
@@ -55,25 +45,11 @@ buttons.forEach((button) => {
   });
 });
 
-// rock.addEventListener('click', function() {
-//     myString = "Rock";
-//     console.log(myString); // Output: Button was clicked!
-//   });
-// console.log(myString);
-
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const random = Math.floor(Math.random() * choices.length);
   return choices[random];
 }
-
-// function getHumanChoice() {
-//   rock.addEventListener("click", function () {
-//     myString = "Rock";
-//     // Output: Button was clicked!
-//     return myString
-//   });
-// }
 
 let humanScore = 0;
 let computerScore = 0;
@@ -116,85 +92,3 @@ function playRound(humanSelection, computerSelection) {
   }
 }
 
-// function playGame() {
-//   let humanScore = 0;
-//   let computerScore = 0;
-
-//   // function playRound(humanSelection, computerSelection) {
-//   //   console.log(
-//   //     `\nYou chose ${humanSelection} computer chose ${computerSelection}`
-//   //   );
-
-//   //   if (humanSelection == "rock" && computerSelection == "paper") {
-//   //     computerScore++;
-//   //     console.log(
-//   //       `You lost that round.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (humanSelection == "rock" && computerSelection == "scissors") {
-//   //     humanScore++;
-//   //     console.log(
-//   //       `You won that round!\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (humanSelection == "rock" && computerSelection == "rock") {
-//   //     console.log(
-//   //       `It's a draw.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (humanSelection == "paper" && computerSelection == "scissors") {
-//   //     computerScore++;
-//   //     console.log(
-//   //       `You lost that round.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (humanSelection == "paper" && computerSelection == "rock") {
-//   //     humanScore++;
-//   //     console.log(
-//   //       `You won that round!\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (humanSelection == "paper" && computerSelection == "paper") {
-//   //     console.log(
-//   //       `It's a draw.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (humanSelection == "scissors" && computerSelection == "rock") {
-//   //     computerScore++;
-//   //     console.log(
-//   //       `You lost that round.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (humanSelection == "scissors" && computerSelection == "paper") {
-//   //     humanScore++;
-//   //     console.log(
-//   //       `You won that round!\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   } else if (
-//   //     humanSelection == "scissors" &&
-//   //     computerSelection == "scissors"
-//   //   ) {
-//   //     console.log(
-//   //       `It's a draw.\nYour score: ${humanScore} \nComputer score: ${computerScore}`
-//   //     );
-//   //   }
-//   // }
-
-//   // Game loop
-//   // do {
-
-//   //   let humanSelection = "";
-//   //   buttons.forEach((button) => {
-//   //     button.addEventListener("click", () => {
-//   //       humanSelection = `${button.className}`;
-//   //       console.log(humanSelection);
-//   //     });
-//   //   });
-
-//   //   let computerSelection = getComputerChoice();
-//   //   playRound(humanSelection, computerSelection);
-//   // } while (computerScore < 5 && humanScore < 5);
-
-//   // Determines winner
-
-//   // if (computerScore > humanScore) {
-//   //   console.log("\nComputer won.");
-//   // } else {
-//   //   console.log("\nYou won!");
-//   // }
-// }
-
-// playGame();
